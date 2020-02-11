1b23da176cab9c58a016f0a027c9f24cc8b31f3d
Soham Mukherjee <smukherjee@perimeterinstitute.ca>
2020-02-11 15:37:09 -0500

    Added prolongation and restriction operators

--- src/Spectral/Utilities/BasisTransform1D.jl
+++ src/Spectral/Utilities/BasisTransform1D.jl
@@ -4,7 +4,7 @@
 # Basis change functions
 #--------------------------------------------------------------------
 
-export linetransform, basistransform
+export linetransform, basistransform, restrict, prolongate
 
 function cheb(m::Int, x::Number)::Number
     if abs(x) <= 1
@@ -16,7 +16,7 @@ function cheb(m::Int, x::Number)::Number
     end
 end
 
-function prefactor(i::Int, N::Int)::Number
+function scaling(i::Int, N::Int)::Number
     (i == 1 || i == N+1) ? (return 1/2) : (return 1)
 end
 
@@ -24,7 +24,7 @@ function linetransform(PS::Chebyshev{Tag, N, T}, modal::Array{T, 1}, nodal::Arra
     PSC   = ChebyshevGL{Tag,N,T}(range(PS)...)
     for index in CartesianIndices(nodal)
         for ordr in 0:order(PSC) 
-            nodal[index] += prefactor(ordr+1, order(PSC))*cheb(ordr, naturalcollocation(PSC, index.I[1]))*modal[ordr+1] 
+            nodal[index] += scaling(ordr+1, order(PSC))*cheb(ordr, naturalcollocation(PSC, index.I[1]))*modal[ordr+1] 
         end
     end
     return nodal
@@ -33,7 +33,7 @@ end
 function linetransform(PS::ChebyshevGL{Tag, N, T}, nodal::Array{T, 1}, modal::Array{T,1})::Array{T, 1} where {Tag, N, T}
     for ordr in 0:order(PS) 
         for index in CartesianIndices(modal)
-            modal[ordr+1] += prefactor(index.I[1], order(PS))*cheb(ordr, naturalcollocation(PS, index.I[1]))*nodal[index] 
+            modal[ordr+1] += scaling(index.I[1], order(PS))*cheb(ordr, naturalcollocation(PS, index.I[1]))*nodal[index] 
         end
     end
     return (2/order(PS))*modal
@@ -50,3 +50,24 @@ function basistransform(u::Field{Chebyshev{Tag, N, T}})::Field{ChebyshevGL{Tag, 
     nodal = zeros(T, size(modal))
     return Field(ChebyshevGL{Tag, N, T}(range(u.space)...), linetransform(u.space, modal, nodal))
 end
+
+function restrict(u::Field{Chebyshev{Tag, N1, T}}, N2::Int)::Field{Chebyshev{Tag, N2, T}} where {Tag, N1, T}
+    @assert N2 <= N1
+    return Field(Chebyshev{Tag, N2, T}(range(u.space)...), u.value[1:N2])
+end
+
+function prolongate(u::Field{Chebyshev{Tag, N1, T}}, N2::Int)::Field{Chebyshev{Tag, N2, T}} where {Tag, N1, T}
+    @assert N2 >= N1
+    v = zeros(T, N2)
+    v[1:N1] = u.value
+    return Field(Chebyshev{Tag, N2, T}(range(u.space)...), v)
+end
+
+function restrict(u::Field{ChebyshevGL{Tag, N1, T}}, N2::Int)::Field{ChebyshevGL{Tag, N2, T}} where {Tag, N1, T}
+    return basistransform(restrict(basistransform(u), N2))
+end
+
+function prolongate(u::Field{ChebyshevGL{Tag, N1, T}}, N2::Int)::Field{ChebyshevGL{Tag, N2, T}} where {Tag, N1, T}
+    return basistransform(prolongate(basistransform(u), N2))
+end
+
